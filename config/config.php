76d0dc83b6d10f2714f9f4e88360847f0d35bb09
Gersch Schiff <CrownMedia@GMac.local>
2019-06-15 01:22:07 -0400

    added config.php to .ignore

--- config/config.php
+++ config/config.php
@@ -1,9 +1,8 @@
-
 <?php
 
 	// Midiel: Needed for navbar_m, we can modified later on. Just ideas
 
-	define('ROOT_URL', 'http://localhost/geektext_project/');		// need to modify to match your directory
+	define('ROOT_URL', 'http://localhost:8888/');		// need to modify to match your directory
 	//define('DB_HOST', 'localhost');
 	//define('DB_USER', 'root');
 	//define('DB_PASS', '123456');
