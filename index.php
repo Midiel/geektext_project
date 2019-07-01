fab568b1fc411cc8db7bde5cb4f9c425fdb914e4
Gersch Schiff <propro@GPro.local>
2019-06-30 23:47:22 -0400

    removed imageModal link

--- index.php
+++ index.php
@@ -148,7 +148,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/imageModal.js"></script>
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/rating.css">
 
