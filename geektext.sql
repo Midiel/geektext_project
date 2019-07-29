a107ce6df1df04c376cb0a5aae959380c0ef84e7
Midiel Rodriguez <midiel305@gmail.com>
2019-07-29 14:25:21 -0400

    added saved procedures queries

--- geektext.sql
+++ geektext.sql
@@ -29,7 +29,7 @@ CREATE TABLE credit_card (card_id int AUTO_INCREMENT, user_id int NOT NULL,
 	
 -- create book's table
 CREATE TABLE book (book_id int AUTO_INCREMENT, title VARCHAR(60) NOT NULL,
-	subtitle VARCHAR(60), author VARCHAR(60) NOT NULL, publisher VARCHAR(60),
+	subtitle VARCHAR(60), authors VARCHAR(60) NOT NULL, publisher VARCHAR(60),
 	published_date VARCHAR(20), isbn_13 VARCHAR(20), isbn_10 VARCHAR(20),
 	page_count VARCHAR(10), print_type VARCHAR(20), category VARCHAR(20),
 	average_rating VARCHAR(20), rating_count VARCHAR(20), language VARCHAR(20),
@@ -46,9 +46,164 @@ CREATE TABLE cart (cart_id int AUTO_INCREMENT, user_id int NOT NULL,
 	FOREIGN KEY (book_id) REFERENCES book(book_id) ON DELETE CASCADE);
 
 	
-	
-	
-	
+
+-- *****Note: You MUST modify these procedures with your database credentials (our username: cen_team8) before running these queries.
+
+-- procedure to add an address
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `addAddress`(IN `token` VARCHAR(60), IN `name` VARCHAR(60), 
+	IN `street` VARCHAR(60), IN `state` VARCHAR(60), IN `city` VARCHAR(60), IN `zip` VARCHAR(60), IN `country` VARCHAR(60))
+	INSERT INTO address (name, street_address, state, city, zip_code, country, primary_phone, user_id) VALUES 
+	(name, street, state, city, zip, country, '3055555555', 
+	(SELECT user_id 
+				FROM user 
+				WHERE user.token = token))$$
+DELIMITER ;
+
+
+-- procedure to add book to the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `addToCart`(IN `token` VARCHAR(60), IN `book_id` INT, IN `qty` INT)
+	BEGIN
+		IF book_id IN (SELECT book_id
+					FROM cart
+					WHERE cart.book_id = book_id
+					AND user_id IN (SELECT user_id
+									FROM user
+									WHERE user.token = token))
+			THEN UPDATE cart SET cart.qty = cart.qty + qty, saved_for_later = 0
+						WHERE cart.book_id = book_id 
+						AND user_id IN (SELECT user_id 
+										FROM user 
+										WHERE user.token = token);
+		ELSE INSERT INTO cart (book_id, qty, saved_for_later, user_id, price) 
+			VALUES (book_id, qty, 0, (SELECT user.user_id 
+										FROM user 
+										WHERE user.token = token), 
+												(SELECT book.price
+												FROM book
+												WHERE book.book_id = book_id));
+		
+		END IF;
+	END$$
+DELIMITER ;
+
+
+-- to add a credit card to the user's account
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `addCard`(IN `token` VARCHAR(60), IN `holder` VARCHAR(100), 
+	IN `type` VARCHAR(20), IN `number` VARCHAR(16), IN `exp` INT(11), IN `year` INT(11), IN `code` INT(11))
+	INSERT INTO credit_card (cardholder, type, number, exp_month, exp_year, security_code, user_id) VALUES 
+	(holder, type, number, exp, year, code, (SELECT user_id 
+				FROM user 
+				WHERE user.token = token))$$
+DELIMITER ;
+
+
+-- changes quanty of books in the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `changeQty`(IN `token` VARCHAR(60), IN `id` INT, IN `quantity` INT)
+	UPDATE cart SET qty = quantity 
+	WHERE book_id = id
+	AND user_id IN (SELECT user_id 
+					FROM user 
+					WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- deletes a book from the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `deleteFromCart`(IN `token` VARCHAR(60), IN `book_id` INT)
+	DELETE FROM cart 
+	WHERE cart.book_id = book_id
+	AND user_id IN (SELECT user_id 
+					FROM user 
+					WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- removes all books from the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `emptyCart`(IN `token` VARCHAR(60))
+	DELETE FROM cart
+	WHERE saved_for_later = 0
+	AND user_id IN (SELECT user_id 
+					FROM user 
+					WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- returns all records/books in the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `getCart`(IN `token` VARCHAR(60))
+	SELECT title, authors, image_url, cart.qty, cart.price, cart.book_id, saved_for_later
+	FROM cart, book 
+	WHERE book.book_id = cart.book_id
+	AND cart.user_id IN (SELECT user_id 
+						FROM user
+						WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- returns all books/records in the saved for later list
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `getSavedForLater`(IN `token` VARCHAR(60))
+	SELECT title, author, image_url, cart.qty, cart.price, cart.book_id, saved_for_later
+	FROM cart, book 
+	WHERE book.book_id = cart.book_id
+	AND saved_for_later = 1
+	AND cart.user_id IN (SELECT user_id 
+						FROM user
+						WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- to get a subtotal of the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `getCartSubtotal`(IN `token` VARCHAR(60))
+	SELECT SUM(qty) as NumItems, SUM(qty * price) as Subtotal
+	FROM cart
+	WHERE saved_for_later = 0
+	AND cart.user_id IN (SELECT user_id 
+						FROM user
+						WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- get number of books in the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `getCartQty`(IN `token` VARCHAR(60))
+	SELECT SUM(qty) as number_of_items
+	FROM cart
+	WHERE saved_for_later = 0
+	AND cart.user_id IN (SELECT user_id 
+						FROM user
+						WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- moves book from saved fro later list to the shopping cart
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `moveToCart`(IN `token` VARCHAR(60), IN `book_id` INT)
+	UPDATE cart 
+	SET saved_for_later = 0, qty = 1
+	WHERE cart.book_id = book_id
+	AND user_id IN (SELECT user_id 
+					FROM user 
+					WHERE user.token = token)$$
+DELIMITER ;
+
+
+-- moves books from the shopping cart to the saved for later list
+DELIMITER $$
+	CREATE DEFINER=`cen_team8`@`localhost` PROCEDURE `saveForLater`(IN `token` VARCHAR(60), IN `book_id` INT)
+	UPDATE cart 
+	SET saved_for_later = 1, qty = 0
+	WHERE cart.book_id = book_id
+	AND user_id IN (SELECT user_id 
+					FROM user 
+					WHERE user.token = token)$$
+DELIMITER ;
 	
 	
 	
